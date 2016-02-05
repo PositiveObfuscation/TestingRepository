3bbeb4c874a45c254fa3cdf919426d275337c4b9
mihalikv <mihalik.vilko@gmail.com>
2016-02-05 21:12:16 +0100

    init2

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,7 @@
 /**
- * Created by Vilo on 5.2.2016.
+ * Created by Jakub Brindza a Viliam Mihalik on 5.2.2016.
  */
 public class Main {
-
     public static void main(String[] args) {
         System.out.println("WUAAA");
     }
